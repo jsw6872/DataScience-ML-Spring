e9ec8d38d8b4087f8fd83efeb002d9aee38a9e8b
seong woo <jsw6872@gmail.com>
2022-09-21 00:37:37 +0900

    update example of monte carlo method

---
+++ DL/monte_carlo.py
@@ -0,0 +1,44 @@
+from numpy import random
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+import numpy as np
+import collections
+
+def sigmoid_func(x):
+    return 1/(1+np.exp(-x))
+
+
+def figure_sigmoid_scatter(iterations):
+    # 시그모이드 그리기 
+    x = np.arange(-5, 5.1, 0.1)
+    y = sigmoid_func(x)
+
+    fig, ax = plt.subplots(figsize=(12,8))
+
+    plt.plot(x, y, color = 'k')
+    plt.grid(True)
+    plt.axis([-6, 6, -0.1, 1.1])
+
+    ax.add_patch(patches.Rectangle((-5,0), 10, 1, fill = False, linewidth = 2)) 
+    
+    # scatter 찍기
+    random_x = random.uniform(-5, 5, size = iterations)
+    random_y = random.uniform(0, 1, size = iterations)
+
+    color = list(map(lambda x: 'blue' if x else 'red', random_y > sigmoid_func(random_x)))
+
+    inner_ratio = collections.Counter(color)['red'] / len(color)
+
+    plt.scatter(random_x, random_y, color = color, s = 2, label = f'{inner_ratio * 10}')
+    plt.legend()
+
+    plt.show()
+    
+
+
+if __name__ == '__main__':
+    iterations = 100000
+
+    figure_sigmoid_scatter(iterations)
